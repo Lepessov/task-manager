488f627f49dfe225a8933281f9c6fcd0ff518583
Daulet <lepessov.daulet@gmail.com>
2024-12-15 19:54:46 +0500

    messages

--- task-manager/app/Http/Requests/UpdateTaskRequest.php
+++ task-manager/app/Http/Requests/UpdateTaskRequest.php
@@ -21,5 +21,30 @@ class UpdateTaskRequest extends FormRequest
             'deadline' => 'nullable|date',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'title.nullable' => 'Заголовок задачи может быть пустым.',
+            'title.string' => 'Заголовок задачи должен быть строкой.',
+            'title.max' => 'Заголовок задачи не может превышать 255 символов.',
+
+            'description.nullable' => 'Описание задачи может быть пустым.',
+            'description.string' => 'Описание задачи должно быть строкой.',
+
+            'status.nullable' => 'Статус задачи может быть пустым.',
+            'status.string' => 'Статус задачи должен быть строкой.',
+            'status.in' => 'Статус задачи должен быть одним из: TODO, IN_PROGRESS, COMPLETED.',
+
+            'importance.nullable' => 'Важность задачи может быть пустой.',
+            'importance.integer' => 'Важность задачи должна быть числом.',
+            'importance.min' => 'Важность задачи не может быть меньше 1.',
+            'importance.max' => 'Важность задачи не может быть больше 5.',
+
+            'deadline.nullable' => 'Дата дедлайна может быть пустой.',
+            'deadline.date' => 'Дата дедлайна должна быть корректной датой.',
+        ];
+    }
+
 }
 
